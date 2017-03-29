ec01e89a20266fd28b2bfbc4649f0267f857f001
pradeepinmar <pradeep.macharla@inmar.com>
2017-03-29 16:20:55 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+    mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
+var app = express();
+
+var port  = process.env.PORT || 3000;
+
+var bookRouter =  express.Router();
+
+bookRouter.route('/Books')
+    .get(function(req,res){
+        Book.find(function(err,books){
+            if(err)
+            res.status(500).send(err);
+            else
+            res.json(books);
+        })
+    })
+
+app.use('/api',bookRouter);
+
+app.get('/', function(req,res){
+    res.send('Welcome to my api');
+});
+
+app.listen(port, function(){
+    console.log("Gulp is running on port:" + port)
+})
