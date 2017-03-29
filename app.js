d9e0ea0d662607bce2b5c96373e56f76a466f3d9
Pradeep <pradeep@seleniumframework.com>
2017-03-29 18:21:59 -0400

    bodyparser added + POST method for adding book

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 mongoose = require('mongoose');
+bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/bookAPI');
 
@@ -8,9 +9,17 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 var bookRouter = express.Router();
 
 bookRouter.route('/books')
+    .post(function(req, res) {
+        var book = new Book(req.body);
+        console.log(book);
+        res.send(book);
+    })
     .get(function (req, res) {
         
         // Need much less code snippet to achieve the validation below
