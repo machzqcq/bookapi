3529b0b08fbb6d8199108a7b5ff897b0313197a9
Pradeep <pradeep@seleniumframework.com>
2017-03-30 17:07:28 -0400

    separate models into another file, update app.js accordingly

--- app.js
+++ app.js
@@ -12,53 +12,7 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-var bookRouter = express.Router();
-
-bookRouter.route('/books')
-    .post(function(req, res) {
-        var book = new Book(req.body);
-        console.log(book);
-        res.send(book);
-    })
-    .get(function (req, res) {
-        
-        // Need much less code snippet to achieve the validation below
-        var query = {}
-        if(req.query.genre)
-        {
-            query.genre = req.query.genre;
-        }
-        if(req.query.title)
-        {
-            query.title = req.query.title;
-        }
-         if(req.query.author)
-        {
-            query.author = req.query.author;
-        }
-         if(req.query.read)
-        {
-            query.title = req.query.read;
-        }
-
-
-        Book.find(query, function (err, books) {
-            if (err)
-                res.status(500).send(err);
-            else
-                res.json(books);
-        })
-    })
-
-bookRouter.route('/books/:bookId')
-    .get(function (req, res) {
-        Book.findById(req.params.bookId, function (err, book) {
-            if (err)
-                res.status(500).send(err);
-            else
-                res.json(book);
-        })
-    })
+bookRouter = require('./Routes/bookRoutes.js')(Book);
 
 app.use('/api', bookRouter);
 
