63e9dfcf58b3aa47796af908b0d1b7636121149a
Pradeep <pradeep@seleniumframework.com>
2017-03-30 18:31:03 -0400

    fix bug for can't set headers for response if already sent

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -1,72 +1,85 @@
 var express = require('express');
 
-var routes = function(Book){
-var bookRouter = express.Router();
+var routes = function (Book) {
+    var bookRouter = express.Router();
 
-bookRouter.route('/')
-    .post(function(req, res) {
-        var book = new Book(req.body);
-        console.log(book);
-        book.save();
-        res.status(201).send(book);
-    })
-    .get(function (req, res) {
-        
-        // Need much less code snippet to achieve the validation below
-        var query = {}
-        if(req.query.genre)
-        {
-            query.genre = req.query.genre;
-        }
-        if(req.query.title)
-        {
-            query.title = req.query.title;
-        }
-         if(req.query.author)
-        {
-            query.author = req.query.author;
-        }
-         if(req.query.read)
-        {
-            query.title = req.query.read;
-        }
+    bookRouter.route('/')
+        .post(function (req, res) {
+            var book = new Book(req.body);
+            console.log(book);
+            book.save();
+            res.status(201).send(book);
+        })
+        .get(function (req, res) {
 
-        Book.find(query, function (err, books) {
+            // Need much less code snippet to achieve the validation below
+            var query = {}
+            if (req.query.genre) {
+                query.genre = req.query.genre;
+            }
+            if (req.query.title) {
+                query.title = req.query.title;
+            }
+            if (req.query.author) {
+                query.author = req.query.author;
+            }
+            if (req.query.read) {
+                query.title = req.query.read;
+            }
+
+            Book.find(query, function (err, books) {
+                if (err)
+                    res.status(500).send(err);
+                else
+                    res.json(books);
+            })
+        });
+
+    bookRouter.use('/:bookId', function (req, res, next) {
+        Book.findById(req.params.bookId, function (err, book) {
             if (err)
                 res.status(500).send(err);
-            else
-                res.json(books);
-        })
-    })
-
-bookRouter.use('/:bookId', function(req, res, next){
-    Book.findById(req.params.bookId, function(err, book){
-        if (err)
-                res.status(500).send(err);
-            else if(book)
-            {
+            else if (book) {
                 req.book = book;
                 next();
             }
-            else
-            {
+            else {
                 res.status(404).send('no book found');
             }
-                res.json(book);
-    })
-});
-bookRouter.route('/:bookId')
-    .get(function (req, res) {
-       res.json(req.book);
-    })
-    .put(function (req, res){
-                req.book.title = req.body.title;
-                req.book.author = req.body.author;
-                req.book.genre = req.body.genre;
-                req.book.read = req.body.read;
-                req.book.save();
-                res.json(req.book);
+        })
     });
+    bookRouter.route('/:bookId')
+        .get(function (req, res) {
+            res.json(req.book);
+        })
+        .put(function (req, res) {
+            req.book.title = req.body.title;
+            req.book.author = req.body.author;
+            req.book.genre = req.body.genre;
+            req.book.read = req.body.read;
+            req.book.save(function (err) {
+                if (err)
+                    res.status(500).send(err);
+                else {
+                    res.json(req.book);
+                }
+            })
+            res.json(req.book);
+        })
+        .patch(function (req, res) {
+            if (req.body._id)
+                delete req.body._id;
+            for (var p in req.body) {
+                req.book[p] = req.body[p];
+            }
+            req.book.save(function (err) {
+                if (err)
+                    res.status(500).send(err);
+                else {
+                    res.json(req.book);
+                }
+            })
+        });
     return bookRouter;
 };
 
