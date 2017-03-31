2e516002547adace5a4ea7971d44612aa5b3db7d
Pradeep <pradeep@seleniumframework.com>
2017-03-30 20:23:35 -0400

    self.links for navigating api

--- Controllers/bookController.js
+++ Controllers/bookController.js
@@ -1,41 +1,50 @@
-var bookController = function(Book){
+var bookController = function (Book) {
 
     var post = function (req, res) {
-            var book = new Book(req.body);
-            console.log(book);
-            book.save();
-            res.status(201).send(book);
-        }
+        var book = new Book(req.body);
+        console.log(book);
+        book.save();
+        res.status(201).send(book);
+    }
 
     var get = function (req, res) {
 
-            // Need much less code snippet to achieve the validation below
-            var query = {}
-            if (req.query.genre) {
-                query.genre = req.query.genre;
-            }
-            if (req.query.title) {
-                query.title = req.query.title;
-            }
-            if (req.query.author) {
-                query.author = req.query.author;
-            }
-            if (req.query.read) {
-                query.title = req.query.read;
+        // Need much less code snippet to achieve the validation below
+        var query = {}
+        if (req.query.genre) {
+            query.genre = req.query.genre;
+        }
+        if (req.query.title) {
+            query.title = req.query.title;
+        }
+        if (req.query.author) {
+            query.author = req.query.author;
+        }
+        if (req.query.read) {
+            query.title = req.query.read;
+        }
+
+        Book.find(query, function (err, books) {
+            if (err)
+                res.status(500).send(err);
+            else {
+                var returnBooks = [];
+                books.forEach(function(element, index, array){
+                    var newBook = element.toJSON();
+                    newBook.links = {};
+                    newBook.links.self = 'http://' + req.headers.host + '/api/books' + newBook._id;
+                    returnBooks.push(newBook);
+                });
+                res.json(returnBooks);
             }
 
-            Book.find(query, function (err, books) {
-                if (err)
-                    res.status(500).send(err);
-                else
-                    res.json(books);
-            });
-        }
-        return {
-            post: post,
-            get: get
-        
-        }
+        });
+    }
+    return {
+        post: post,
+        get: get
+
+    }
 }
 
 module.exports = bookController;
